e17c543d00372c1c846b1337e3fccdc96a89f926
cengamat <azurnaci@cultwinesltd.com>
2023-05-01 21:05:42 +0100

    First test

---
+++ tests/home.js
@@ -0,0 +1,14 @@
+module.exports = {
+  //   after: function (browser) {
+  //     console.log("I am done.");
+  //   },
+  "Demo test": function (browser) {
+    browser
+      .url(browser.launch_url)
+      .waitForElementVisible("[data-nw=name-input]")
+      .setValue("[data-nw=name-input]", "Pierre")
+      .weirdPause(browser.globals.timeToWait)
+      .assert.textContains("[data-nw=welcome-message]", "Welcome Pierre !")
+      .end();
+  },
+};
